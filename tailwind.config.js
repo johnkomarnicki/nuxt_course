a2d5354d3021b9cb03abca7dbb03a3dc6a89b5f2
John <john@johnkomarnicki.com>
2024-06-19 12:21:33 -0400

    add starting files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  theme: {
+    extend: {
+      colors: {},
+    },
+    fontFamily: {},
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+  },
+};
