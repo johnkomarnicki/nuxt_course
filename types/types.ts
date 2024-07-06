baef6ca4c775ad54ff5edd9826175cfebce6aa1d
John <john@johnkomarnicki.com>
2024-07-06 09:43:14 -0400

    fix: update starting files

---
+++ types/types.ts
@@ -0,0 +1,27 @@
+export interface ApiResponse {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
