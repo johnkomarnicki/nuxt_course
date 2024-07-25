e89c8ea079fd40fc6b22b4bb91c995f713af85b9
John <john@johnkomarnicki.com>
2024-07-25 16:25:24 -0400

    completed project

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -4,7 +4,7 @@
   <header class="py-4 border-b">
     <nav class="flex container items-center">
       <NuxtLink to="/" class="flex gap-1 items-center">
-        <NuxtImg width="56px" src="/icon-green.png" alt="" />
+        <img class="w-14" src="/icon-green.png" alt="" />
         <span class="text-3xl font-bold">Nuxtcipes</span>
       </NuxtLink>
       <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
