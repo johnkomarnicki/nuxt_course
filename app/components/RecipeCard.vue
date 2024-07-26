87651c11344aadb524d6d8edc670e990b102360c
John <john@johnkomarnicki.com>
2024-07-26 11:57:46 -0400

    remove nuxt features

--- app/components/RecipeCard.vue
+++ app/components/RecipeCard.vue
@@ -7,30 +7,29 @@ defineProps<{
 </script>
 
 <template>
-  <div class="shadow rounded-md">
+  <div class="flex flex-col shadow rounded-md">
     <img :src="recipe.image" alt="" class="rounded-t-md" />
-    <div class="flex flex-col py-6 px-4">
-      <p class="text-2xl font-semibold mb-2">{{ recipe.name }}</p>
-      <div class="font-normal w-full bg-white/80 flex gap-8 text-xl mb-4">
+    <div class="flex flex-col py-6 px-4 flex-1">
+      <p class="text-xl lg:text-2xl font-semibold mb-2">{{ recipe.name }}</p>
+      <div class="font-normal w-full bg-white/80 flex gap-8 text-lg lg:text-xl mb-4 mt-auto">
         <div class="flex items-center gap-1">
-          <Icon
-            name="mdi:clock-time-eight-outline"
-            width="1.2em"
-            height="1.2em"
-            style="color: #f79f1a"
-          />
+          <Icon name="mdi:clock-time-eight-outline" style="color: #f79f1a" />
           <span>{{ recipe.cookTimeMinutes }}</span>
         </div>
         <div class="flex items-center gap-1">
-          <Icon name="mdi:fire" width="1.2em" height="1.2em" style="color: #f79f1a" />
+          <Icon name="mdi:fire" style="color: #f79f1a" />
           <span>{{ recipe.caloriesPerServing }}</span>
         </div>
         <div class="flex items-center gap-1">
-          <Icon name="mdi:star" width="1.2em" height="1.2em" style="color: #f79f1a" />
+          <Icon name="mdi:star" style="color: #f79f1a" />
           <span>{{ recipe.rating }} ({{ recipe.reviewCount }})</span>
         </div>
       </div>
-      <BaseBtn :to="`/recipe/${recipe.id}`" label="View" />
+      <button
+        class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-base lg:text-lg cursor-pointer"
+      >
+        View
+      </button>
     </div>
   </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,26 +7,29 @@ const { data } = await useFetch<RecipeResponse>("https://dummyjson.com/recipes?l
 <template>
   <main>
     <section class="bg-[#f1f1f1]">
-      <div class="container flex items-center py-20">
-        <div class="flex-1">
-          <h1 class="text-6xl font-extrabold mb-6 text-balance">
+      <div class="container flex flex-col lg:flex-row items-center py-20 gap-10">
+        <div class="flex-1 order-2 lg:order-1 text-center lg:text-left">
+          <h1 class="text-4xl lg:text-6xl font-extrabold mb-6 text-balance">
             Master the Kitchen with Ease: Unleash Your Inner Chef Today!
           </h1>
-          <p class="text-2xl mb-8 text-balance">
+          <p class="text-xl lg:text-2xl mb-8 text-balance">
             Discover recipes helping you to find the easiest way to cook.
           </p>
-
-          <BaseBtn label="Browse Recipes" />
+          <button
+            class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-lg cursor-pointer"
+          >
+            Browse Recipes
+          </button>
         </div>
-        <div class="flex-1">
-          <NuxtImg sizes="sm:50vw" src="/nuxt-course-hero.png" alt="" />
+        <div class="flex-1 order-1 lg:order-2">
+          <img src="/nuxt-course-hero.png" alt="" />
         </div>
       </div>
     </section>
     <section class="py-20 container">
-      <h2 class="text-5xl mb-2">Discover, Create, Share</h2>
-      <p class="text-xl mb-8">Check out our most popular recipes!</p>
-      <div class="grid grid-cols-3 gap-x-4 gap-y-8">
+      <h2 class="text-3xl lg:text-5xl mb-2">Discover, Create, Share</h2>
+      <p class="text-lg lg:text-xl mb-8">Check out our most popular recipes!</p>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
         <RecipeCard v-for="recipe in data?.recipes" :recipe="recipe" />
       </div>
     </section>
