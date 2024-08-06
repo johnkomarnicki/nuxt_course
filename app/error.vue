836488e89119415318fe0b917cd94b8a16f1d737
John <john@johnkomarnicki.com>
2024-08-06 09:06:55 -0400

    error page

---
+++ app/error.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+</script>
+
+<template>
+  <section class="flex min-h-screen flex-col items-center justify-center">
+    <h1 class="mb-2 text-9xl font-bold">
+      {{ error?.statusCode }}
+    </h1>
+    <p class="mb-10 text-xl sm:text-3xl">{{ error?.message }}</p>
+    <NuxtLink
+      to="/"
+      class="px-4 py-2 text-white bg-dodgeroll-gold rounded-md text-lg cursor-pointer"
+    >
+      Go home
+    </NuxtLink>
+  </section>
+</template>
