540bf4123785651a9775065fb5c5ababd3bec169
John <komarnickij@yahoo.com>
2024-12-10 15:14:32 -0500

    module 3 starting files

---
+++ app/components/SignupForm.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container max-w-screen-md mx-auto p-10">
+    <h1 class="text-4xl lg:text-5xl mb-4 text-balance text-center font-extrabold">
+      Recipes Straight To Your Inbox
+    </h1>
+
+    <p class="text-xl lg:text-2xl mb-8 text-balance text-center">
+      Discover tasty new recipes in our weekly newsletter!
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
