ca4309c2fa001a75adc16fb9ab83f0c699227617
John <komarnickij@yahoo.com>
2025-01-22 10:01:35 -0500

    login template

---
+++ app/pages/login.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="bg-[#f1f1f1] max-w-screen-sm mx-auto mt-10 space-y-6 rounded-md p-8 text-xs shadow-md"
+  >
+    <h1 class="text-xl sm:text-2xl">Login</h1>
+  </div>
+</template>
