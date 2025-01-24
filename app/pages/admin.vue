1c93c6aa2a7be3c9721a145899364dd47703beae
John <komarnickij@yahoo.com>
2025-01-24 10:57:01 -0500

    admin template

---
+++ app/pages/admin.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const user = useCookie("user");
+</script>
+
+<template>
+  <main>
+    <section class="container py-20">
+      <h1 class="text-3xl mb-6 text-balance">
+        Welcome back, {{ user }}
+      </h1>
+    </section>
+  </main>
+</template>
