008ad8d0d8ae9f8e51ee60a87d81557049606b07
John <komarnickij@yahoo.com>
2025-02-28 16:42:57 -0500

    add confirm page

---
+++ app/pages/confirm.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="min-h-screen grid place-items-center">
+    <div class="flex flex-col items-center gap-2">
+      <UIcon
+        name="i-mdi-loading"
+        class="text-dodgeroll-gold-500 text-5xl animate-spin"
+      />
+      <h1 class="text-5xl">Verifying...</h1>
+    </div>
+  </div>
+</template>
