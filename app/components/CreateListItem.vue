d19358f1bafd8fa6852b4e1443eb0972df4fe6a7
John <komarnickij@yahoo.com>
2025-09-02 15:45:35 -0400

    module 8 starting files

--- app/components/CreateListItem.vue
+++ app/components/CreateListItem.vue
@@ -1,27 +1,13 @@
-<script setup lang="ts">
-const emit = defineEmits(["removeItem"]);
-const props = defineProps<{
-  item: string;
-  index: number;
-}>();
-
-function removeItem() {
-  emit("removeItem", props.index);
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <li class="flex items-center gap-2">
     <span
       class="flex items-center justify-center bg-dodgeroll-gold-500 w-6 h-6 rounded-full text-white text-sm"
     >
-      {{ index + 1 }}
     </span>
-    <span class="text-sm">
-      {{ item }}
-    </span>
+    <span class="text-sm"> </span>
     <UIcon
-      @click="removeItem"
       name="i-mdi-close-circle"
       class="cursor-pointer ml-auto text-dodgeroll-gold-500 duration-300 hover:text-dodgeroll-gold-300 text-xl"
     />
